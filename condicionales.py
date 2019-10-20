be05267d473dc1d34c61a3af4e96afba8233e0b2
lucasBMax <lucas.1701716343@gmail.com>
2019-10-20 01:08:32 -0500

    agregar estados

--- condicionales.py
+++ condicionales.py
@@ -4,4 +4,17 @@ class Condicion:
 
 def condicionales():
 	
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
+	dictEstados = {'A': [0, 1, 2, 3, 4, 5], 'B': [0, 1, 2, 3]}
 	
--- ventana.py
+++ ventana.py
@@ -1,16 +1,18 @@
-import sys, os, time
+import sys, os, time, itertools, pygame
 import networkx as nx
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
 from PyQt5 import uic, QtCore, Qt
 #from grafo import *
 from transicion import *
-#from pygame.locals import *
+from pygame.locals import *
 
 global lista
 global dictEstados
+global Grafo
 dictEstados = {}
 lista = []
+
 class Ventana(QMainWindow):
 	
 	def __init__(self):
@@ -29,18 +31,28 @@ class Ventana(QMainWindow):
 				self.valorVresultado.text(), self.txtCondicion.text()))
 		self.btFin.clicked.connect(lambda: self.generarArchivo(dictEstados))
 		self.btAgregEstados.clicked.connect(lambda: self.agregarEstado())
-		#self.btAgregarTransicion.clicked.connect(lambda: self.agregarTransicion())
-   		# self.btEstadoInicial.clicked.connect(lambda: self.agregarInicial(self.guardarVariables()))
-   		# self.btEstadoAceptacion.clicked.connect(lambda: self.estadosAceptacion())
-   		# self.btFin.clicked.connect(lambda: self.dibujar())
+		self.btFin.clicked.connect(lambda: self.dibujar())
 
-	def agregarEstado(self):
-   		dictEstados[self.nombreVariable.text()] = int(self.valorIni.text())
-   		print(dictEstados)
-	
 	def generarTransicion(self, nombre, origen, destino, condicion):
 		nueva = Transicion(nombre, origen, destino, condicion)
 		lista.append(nueva)
+
+	def agregarEstado(self):
+		listaDiccionario = []
+		listaEstados = []
+		n = 0
+		for i in range(0,(int(self.rango.text()) + 1)):
+			listaDiccionario.append(n)
+			n += 1
+		dictEstados[self.nombreVariable.text()] = listaDiccionario
+		for combo in itertools.product(*[dictEstados[k] for k in sorted(dictEstados.keys())]):
+			listaEstados.append(combo)
+		Grafo = nx.DiGraph()
+		for i in listaEstados:
+			Grafo.add_node(i)
+		nx.draw_shell(Grafo, with_labels=True, node_color='pink', font_size= 5, node_size=500)
+		plt.savefig('graph.png',dpi=(140))#transparent=True, facecolor="w"
+		plt.clf()
 
 	def generarArchivo(self, dictEstados):
 		file = open("condicionales.py", "a")
@@ -57,6 +69,27 @@ class Ventana(QMainWindow):
 			file.write('\n')
 			file.write('	')
 		file.close()
+
+	def dibujar(self):
+		pygame.init()
+		window = pygame.display.set_mode((896,672))
+		pygame.display.set_caption("Grafo completo")
+		My_image = pygame.image.load("graph.png")
+		window.blit(My_image,(0,0))
+		pygame.display.update()
+		plt.clf()
+
+		while True:
+		    for event in pygame.event.get():
+		        if event.type == QUIT:
+		            pygame.quit()
+		            sys.exit()
+		        elif event.type == pygame.KEYDOWN:
+		            if event.key == K_a:
+		                draw_edges()
+		            elif event.key == K_d:
+		                delete_edges()
+		    pygame.display.update()
 #Iniciar Aplicacion
 app=QApplication(sys.argv)
 #Crear objeto clase
